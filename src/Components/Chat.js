53a65aec0e246174d3f838c19c3c82623718ff30
My Chau <nguyenlemychau1201@gmail.com>
2024-04-09 21:32:03 +0700

    Use GiftedChat for Chat.js

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -5,9 +5,79 @@ import { useRef } from 'react';
 import { useState } from 'react';
 import { StyleSheet, View, Button, Text, TextInput } from 'react-native';
 import axios from 'axios';
+import { GiftedChat, Bubble, Actions } from 'react-native-gifted-chat';
+
 
 export default function Chat({ navigation }) {
     const [mess, setMess] = useState('')
+    const [currentMessage, setCurrentMessage] = useState('');
+    const [messages, setMessages] = useState([
+        {
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+                _id: 2,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any',
+            },
+        },
+        {
+            _id: 2,
+            text: 'Hello world',
+            createdAt: new Date(),
+            user: {
+                _id: 1,
+                name: 'Developer',
+                avatar: 'https://placeimg.com/140/140/any',
+            },
+        },
+    ]);
+
+    function onSend() {
+        if (currentMessage.length > 0) {
+            setMessages(previousMessages => GiftedChat.append(previousMessages, {
+                _id: previousMessages.length + 1,
+                text: currentMessage,
+                createdAt: new Date(),
+                user: {
+                    _id: 1,
+                },
+            }));
+            setCurrentMessage('');
+        }
+    }
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    left: {
+                        backgroundColor: 'white'
+                    }
+                }}
+            />
+        );
+    }
+
+    function onLongPress(context, message) {
+        console.log(context, message);
+        const options = ['Copy Message','Delete Message', 'Forward Message', 'Cancel'];
+        const cancelButtonIndex = options.length - 1;
+        context.actionSheet().showActionSheetWithOptions({
+            options,
+            cancelButtonIndex
+        }, (buttonIndex) => {
+            switch (buttonIndex) {
+                case 0:
+                    // Your delete logic
+                    break;
+                case 1:
+                    break;
+            }
+        });
+    }
 
     return (
         <View style={styles.container}>
@@ -49,6 +119,21 @@ export default function Chat({ navigation }) {
                 />
             </View>
 
+            <GiftedChat
+                messages={messages}
+                onSend={newMessages => onSend(newMessages)}
+                user={{
+                    _id: 1,
+                }}
+                renderBubble={renderBubble}
+                listViewProps={{
+                    style: { top: -50 }
+                }}
+                onPress={(context, message) => onLongPress(context, message)}
+
+            />
+
+
             <View style={styles.chat}>
 
                 <MaterialCommunityIcons
@@ -57,13 +142,13 @@ export default function Chat({ navigation }) {
                     color="black" style={{ padding: 5 }}
                 />
 
-                {mess === '' ? (
+                {currentMessage === '' ? (
                     <>
                         <TextInput
                             placeholder="Tin nhắn"
                             style={styles.txtSDT}
-                            value={mess}
-                            onChangeText={(text) => setMess(text)}
+                            value={currentMessage}
+                            onChangeText={(text) => setCurrentMessage(text)}
                             keyboardType="phone-pad"
                         />
 
@@ -93,8 +178,8 @@ export default function Chat({ navigation }) {
                         <TextInput
                             placeholder="Tin nhắn"
                             style={styles.txtSDTFocus}
-                            value={mess}
-                            onChangeText={(text) => setMess(text)}
+                            value={currentMessage}
+                            onChangeText={(text) => setCurrentMessage(text)}
                             keyboardType="phone-pad"
                         />
 
@@ -102,6 +187,7 @@ export default function Chat({ navigation }) {
                             name="send"
                             size={30}
                             color="#006AF5" style={{ marginLeft: 20 }}
+                            onPress={onSend}
                         />
 
                     </>
@@ -129,7 +215,7 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: 'white',
         position: 'absolute',
-        bottom: 0,
+        bottom: 0
     },
 
     txtSDT: {
