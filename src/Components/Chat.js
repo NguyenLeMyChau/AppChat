e067c1770a78a53affaa73e578d7be15902fcb9d
My Chau <nguyenlemychau1201@gmail.com>
2024-04-11 17:43:07 +0700

    Upload Image

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,24 +1,27 @@
 
-import { AntDesign, MaterialCommunityIcons, SimpleLineIcons, Entypo,Feather } from '@expo/vector-icons';
+import { AntDesign, MaterialCommunityIcons, SimpleLineIcons, Entypo, Feather } from '@expo/vector-icons';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
 import axios from 'axios';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import io, { Socket } from "socket.io-client";
+import EmojiSelector from 'react-native-emoji-selector';
 
-export default function Chat({ navigation,route }) {
-    
+
+export default function Chat({ navigation, route }) {
     const { friend } = route.params;
     var userId;
-    const [mess, setMess] = useState('')
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
     const [userData, setUserData] = useState({});
     const [currentMessage, setCurrentMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const [socket, setSocket] = useState(null); 
-    
-    
+    const [socket, setSocket] = useState(null);
+    const [img, setImg] = useState(null)
+    var uploadedImage = null;
+
 
     async function getData() {
         try {
@@ -33,12 +36,7 @@ export default function Chat({ navigation,route }) {
             console.error('Error getting data from AsyncStorage:', error);
         }
     }
-    
-    
-    useEffect(() => {
-        getData();
-        
-    }, []);
+
 
     useEffect(() => {
         getData();
@@ -48,7 +46,7 @@ export default function Chat({ navigation,route }) {
         });
         newSocket.on('sendDataServer', (message) => {
             console.log(userId)
-            if(message.data.user._id===userId|| message.data.user._id===friend._id){
+            if (message.data.user._id === userId || message.data.user._id === friend._id) {
                 setMessages(previousMessages => GiftedChat.append(previousMessages, message.data));
             }
         });
@@ -57,62 +55,122 @@ export default function Chat({ navigation,route }) {
             newSocket.disconnect();
         };
     }, []);
+
     const generateRandomId = (text) => {
         const timestamp = Date.now().toString(); // Thời gian hiện tại dưới dạng chuỗi số
         const randomText = Math.random().toString(36).substring(7); // Chuỗi ngẫu nhiên từ 7 ký tự
         return `${timestamp}-${text}-${randomText}`; // Kết hợp thời gian, văn bản và chuỗi ngẫu nhiên
     };
+
     const onSend = async (messages = []) => {
-      if (currentMessage.length > 0 && socket) {
-        // Kiểm tra socket đã sẵn sàng
-        const newMessage = {
-          _id: generateRandomId(currentMessage),
-          text: currentMessage,
-          createdAt: new Date(),
-          user: {
-            _id: userData._id, // ID của người gửi tin nhắn
-            avatar:userData.avatar
-          },
-        };
-        socket.emit("sendDataClient", newMessage); // Gửi tin nhắn qua Socket.IO
+        if (currentMessage.length > 0 && socket) {
 
-        const response = await axios.post("http://localhost:4000/addmsg", {
-          from: userData._id,
-          to: friend._id,
-          message: currentMessage,
-        });
+            if (img !== null) {
+                uploadedImage = await handleUpImage(img); // Tải tệp lên máy chủ
+                // Tạo tin nhắn chứa URL của tệp
+                const newMessage = {
+                    _id: generateRandomId(currentMessage),
+                    text: uploadedImage, // URL của tệp
+                    createdAt: new Date(),
+                    user: {
+                        _id: userData._id,
+                        avatar: userData.avatar
+                    },
+                };
+                socket.emit("sendDataClient", newMessage); // Gửi tin nhắn qua Socket.IO
+                const response = await axios.post("http://localhost:4000/addmsg", {
+                    from: userData._id,
+                    to: friend._id,
+                    message: uploadedImage,
+                });
+                console.log(response.data.message);
+                setImg(null);
+                uploadedImage = null;
+            } else {
+                // Kiểm tra socket đã sẵn sàng
+                const newMessage = {
+                    _id: generateRandomId(currentMessage),
+                    text: currentMessage,
+                    createdAt: new Date(),
+                    user: {
+                        _id: userData._id, // ID của người gửi tin nhắn
+                        avatar: userData.avatar
+                    },
+                };
+                socket.emit("sendDataClient", newMessage); // Gửi tin nhắn qua Socket.IO
+                const response = await axios.post("http://localhost:4000/addmsg", {
+                    from: userData._id,
+                    to: friend._id,
+                    message: currentMessage,
+                });
+                console.log(response.data.message);
+                setCurrentMessage("");
 
-        console.log(response.data);
-        setCurrentMessage("");
-      }
+            }
+
+
+
+        }
     };
-    
-        const fetchMessages = async (userData) => {
-          try {
+
+    const fetchMessages = async (userData) => {
+        try {
             const response = await axios.post('http://localhost:4000/getmsg', {
-              from: userData._id,
-              to: friend._id,
+                from: userData._id,
+                to: friend._id,
             });
             const formattedMessages = response.data.map(msg => ({
                 _id: msg.id, // ID của tin nhắn
                 text: msg.message, // Nội dung tin nhắn
                 createdAt: new Date(msg.createdAt), // Thời gian tạo tin nhắn (định dạng Date)
                 user: {
-                  _id: msg.fromSelf ? userData._id : friend._id, // ID của người gửi tin nhắn
-                  name: msg.fromSelf ? 'You' : friend.name, // Tên của người gửi tin nhắn
+                    _id: msg.fromSelf ? userData._id : friend._id, // ID của người gửi tin nhắn
+                    name: msg.fromSelf ? 'You' : friend.name, // Tên của người gửi tin nhắn
                 },
                 isHidden: !msg.isHidden, // Trạng thái ẩn tin nhắn (nếu có)
-              }));
-              setMessages(formattedMessages);
-                console.log(formattedMessages);
-          } catch (error) {
+            }));
+            setMessages(formattedMessages);
+            console.log(formattedMessages);
+        } catch (error) {
             console.error('Error fetching messages:', error);
-          }
-        };
-    
+        }
+    };
 
-    
+    async function selectFile() {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = e => {
+            const file = e.target.files[0];
+            console.log(file);
+            console.log(file.name);
+            setImg(file);
+            setCurrentMessage(file.name); // Lưu đối tượng chứa tên và tệp
+        }
+        input.click();
+    };
+
+    async function handleUpImage(file) {
+        const formData = new FormData();
+        formData.append('avatar', file);
+
+        if (file !== null) {
+            const responseAvatar = await axios.post(`http://localhost:4000/user/uploadAvatarS3/${userId}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            const image = responseAvatar.data.avatar;
+            console.log(image);
+            return image;
+        }
+
+    };
+
+
     const renderBubble = (props) => {
+        const isImageMessage = props.currentMessage.text.startsWith("http://") || props.currentMessage.text.startsWith("https://");
+        console.log('isImageMessage:', props.currentMessage);
+
         return (
             <Bubble
                 {...props}
@@ -121,13 +179,24 @@ export default function Chat({ navigation,route }) {
                         backgroundColor: 'white'
                     }
                 }}
+
+                renderMessageImage={isImageMessage ? () => (
+                    <Image
+                        source={{ uri: props.currentMessage.text }}
+                        style={{ width: 250, height: 250 }}
+                    />
+                ) : null}
+                
             />
         );
     }
 
+
+
+
     function onLongPress(context, message) {
         console.log(context, message);
-        const options = ['Copy Message','Delete Message', 'Forward Message', 'Cancel'];
+        const options = ['Copy Message', 'Delete Message', 'Forward Message', 'Cancel'];
         const cancelButtonIndex = options.length - 1;
         context.actionSheet().showActionSheetWithOptions({
             options,
@@ -150,45 +219,53 @@ export default function Chat({ navigation,route }) {
                 paddingHorizontal: 16,
                 paddingVertical: 8,
             }}>
-                <TouchableOpacity style={{width:'10%'}}  onPress={()=>navigation.goBack()}>
-            <AntDesign name="arrowleft" size={20} color="white" />
-          </TouchableOpacity>   
-          <TouchableOpacity style={{width:'55%'}}>         
-                <Text style={{color:'white',fontWeight:'bold',fontSize:18}}>{friend.name}</Text>
-                <Text style={{color:'white',fontSize:13}}>Truy cập</Text>
-          </TouchableOpacity> 
-          <TouchableOpacity style={{width:'13%'}}>
-            <MaterialCommunityIcons name="phone" size={20} color="white" />
-          </TouchableOpacity>   
-          <TouchableOpacity style={{width:'13%'}}>
-            <Feather name="video" size={20} color="white" />
-          </TouchableOpacity>   
-          <TouchableOpacity style={{width:'13%'}}>
-            <SimpleLineIcons name="list" size={20} color="white" />
-          </TouchableOpacity>   
+                <TouchableOpacity style={{ width: '10%' }} onPress={() => navigation.goBack()}>
+                    <AntDesign name="arrowleft" size={20} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ width: '55%' }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 18 }}>{friend.name}</Text>
+                    <Text style={{ color: 'white', fontSize: 13 }}>Truy cập</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ width: '13%' }}>
+                    <MaterialCommunityIcons name="phone" size={20} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ width: '13%' }}>
+                    <Feather name="video" size={20} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ width: '13%' }}>
+                    <SimpleLineIcons name="list" size={20} color="white" />
+                </TouchableOpacity>
             </View>
 
-            <GiftedChat
-                messages={messages}
-                onSend={newMessages => onSend(newMessages)}
-                user={{
-                    _id: userData._id,
-                }}
-                renderBubble={renderBubble}
-                listViewProps={{
-                    style: { top: -50 }
-                }}
-                onPress={(context, message) => onLongPress(context, message)}
+            <View style={{ flex: 1 }}>
+                <GiftedChat
+                    messages={messages}
+                    onSend={newMessages => onSend(newMessages)}
+                    user={{
+                        _id: userData._id,
+                    }}
+                    renderBubble={renderBubble}
+                    listViewProps={{
+                        style: { top: -50 }
+                    }}
+                    onPress={(context, message) => onLongPress(context, message)}
+                    renderInputToolbar={() => null} // Thêm dòng này
 
-            />
+                />
+
+                {showEmojiPicker && <EmojiSelector onEmojiSelected={emoji => setCurrentMessage(currentMessage + emoji)} />}
+
+            </View>
+
 
 
             <View style={styles.chat}>
 
                 <MaterialCommunityIcons
-                    name="sticker-emoji"
-                    size={30}
-                    color="black" style={{ padding: 5 }}
+                    name="emoticon"
+                    size={24}
+                    color="black"
+                    onPress={() => setShowEmojiPicker(!showEmojiPicker)}
                 />
 
                 {currentMessage === '' ? (
@@ -201,18 +278,15 @@ export default function Chat({ navigation,route }) {
                             keyboardType="phone-pad"
                         />
 
-                        <Entypo
-                            name="dots-three-horizontal"
-                            size={24}
-                            color="black"
-                            style={{ marginLeft: 5 }}
-                        />
+                        <AntDesign name="addfile" size={24} color="black" style={{ marginLeft: 5 }} />
 
                         <SimpleLineIcons
                             name="microphone"
                             size={24}
                             color="black"
                             style={{ marginLeft: 20 }}
+                            onPress={() => { handleUpImage(img) }}
+
                         />
 
                         <AntDesign
@@ -220,6 +294,9 @@ export default function Chat({ navigation,route }) {
                             size={30}
                             color="black"
                             style={{ marginLeft: 20 }}
+                            onPress={() => {
+                                selectFile();
+                            }}
                         />
                     </>
                 ) : (
@@ -236,7 +313,7 @@ export default function Chat({ navigation,route }) {
                             name="send"
                             size={30}
                             color="#006AF5" style={{ marginLeft: 20 }}
-                            onPress={()=>onSend()}
+                            onPress={() => onSend()}
                         />
 
                     </>
